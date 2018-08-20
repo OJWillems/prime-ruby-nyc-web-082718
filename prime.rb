69783208f1877f1db609faa3f66f4e41232ea002
Olivier Willems <ojwillems@gmail.com>
2018-08-20 22:47:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 2
+    return false
+  else
+    [*(2...integer)].each do |x|
+      if integer % x == 0
+        return false
+      end
+    end
+  return true
+  end
+end
